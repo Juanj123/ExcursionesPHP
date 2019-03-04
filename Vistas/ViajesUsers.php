ed2e2930b1acf8c726bf07fbf4657a5a95306a43
Juanjo <juan.3395@hotmail.com>
2019-03-04 05:09:07 -0600

    vistas de usuario

---
+++ Vistas/ViajesUsers.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Viajes</title>
+     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="CSS/bootstrap.min.css">
+    <link rel="stylesheet" href="../Vistas/CSS/animate.min.css">
+    <link rel="stylesheet" href="../vistas/CSS/apartaTuLugar.css" />
+        <link href="../Vistas/JS/bootstrap.min.js" rel="stylesheet" type="text/css" />
+</head>
+<body>
+     <div class="container-fluid">
+        <div class="row">
+            <fieldset class="col-12">
+               <legend>Buscar</legend>
+                 <p>
+
+                 <input type="checkbox" name="cb-autos" value="gusta"> Destino
+
+                 <input type="checkbox" name="cb-deportes" value="gusta"> Mes
+
+                 <input type="checkbox" name="cb-videojuegos" value="gusta">Año
+
+                 </p>
+            </fieldset>
+        </div>
+      </div>
+    
+</body>
+</html>
