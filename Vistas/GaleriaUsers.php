eac4c5b29c5d9aae978a08fe74c470231cf9bea8
Juanjo <juan.3395@hotmail.com>
2019-03-12 01:02:19 -0600

    galeria users

--- Vistas/GaleriaUsers.php
+++ Vistas/GaleriaUsers.php
@@ -2,7 +2,7 @@
 <body>
 	<br>
 	<br>
-	
+
    <link href=" https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.css" rel="styleshee"/>
     <link href="../Vistas/CSS/estilosGaleria.css" rel="stylesheet" />
 
@@ -17,24 +17,24 @@
         <div class="row mb-3">
             <div class="col-md-4">
                 <div class="card">
-                    <a class="lightbox" href="img/park.jpg">
-                    <img src="img/park.jpg" alt="Park" class="card-img-top">
+                    <a class="lightbox" href="img/33664107_10204985976927359_6836652645973753856_n.jpg">
+                    <img src="img/33664107_10204985976927359_6836652645973753856_n.jpg" alt="Park" class="card-img-top">
                     </a>
                 </div>
             </div>
              
             <div class="col-md-4">
                 <div class="card">
-                    <a class="lightbox" href="img/benches.jpg">
-                    <img src="img/benches.jpg" alt="Park" class="card-img-top">
+                    <a class="lightbox" href="img/46427477_10205656207402702_4998537502349328384_n.jpg">
+                    <img src="img/46427477_10205656207402702_4998537502349328384_n.jpg" alt="Park" class="card-img-top">
                     </a>
                 </div>
             </div>
              
             <div class="col-md-4">
                 <div class="card">
-                    <a class="lightbox" href="img/bridge.jpg">
-                    <img src="img/bridge.jpg" alt="Park" class="card-img-top">
+                    <a class="lightbox" href="img/47301056_2279072158829965_5230059007225364480_n.jpg">
+                    <img src="img/47301056_2279072158829965_5230059007225364480_n.jpg" alt="Park" class="card-img-top">
                     </a>
                 </div>
             </div>
@@ -42,7 +42,7 @@
           <div class="row"> 
             <div class="col-md-4">
                 <div class="card">
-                    <a class="lightbox" href="img/coast.jpg">
+                    <a class="lightbox" href="img/">
                     <img src="img/coast.jpg" alt="Park" class="card-img-top">
                     </a>
                 </div>
